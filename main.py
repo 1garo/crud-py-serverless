73c7af726f96ecaabf906b67b6d8e4f45c50e8f5
1garo <alevardai@hotmail.com>
2021-03-30 20:05:48 -0300

    feat: create root and create api path

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
 import sqlite3
 import handler as hd
 import db
+
+from flask import Flask, jsonify, render_template, request, redirect, url_for
+app = Flask(__name__,  template_folder='templates')
+
 # use for help -> Bruno Futema
 # and this site below 
 # https://medium.com/@francisco_51376/crud-na-pr%C3%A1tica-com-python-sqlite-8e15d11bbac9
+
+# TODO: continue to rewrite the functions to the web page
+@app.route("/")
+def primeiro_endpoint_get():
+	contacts = db.get_contacts() 
+	a = []
+	contact = {}
+
+	for item in contacts:
+		contact = {"id": item[0], "name": item[1], "cellphone_number": item[2]}
+		a.append(contact)
+
+	return render_template('./index.html', navigation=a)
+
+@app.route("/create", methods=['POST'])
+def create():
+	name = request.form.get("fname","")
+	phone = request.form.get("lphone","")
+	hd.add_new_contact(name, phone)
+	return redirect(url_for('primeiro_endpoint_get'))
 
 def show_header():
   print("-" * 60)
@@ -47,4 +71,6 @@ def init():
       exit(0)
 
 if __name__ == "__main__":
-	init()
+	# init()
+	db.initial_table()
+	app.run(host='0.0.0.0', port=5000, debug=True)
